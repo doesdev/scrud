205fc75859fedfcbc271599cb600edf959114fef
Andrew Carpenter <andrew@audioinhd.com>
2017-02-21 23:26:55 -0500

    Start designing api

--- test/index.js
+++ test/index.js
@@ -1,1 +1,18 @@
 'use strict'
+
+// setup
+import test from 'ava'
+
+// test something
+test.todo('testSomething')
+
+/*
+const pattyOpts = {port: 8081}
+const patty = require('paternity')
+const handleIt = require('./some-other-resource')
+
+await patty.init(pattyOpts)
+await patty.register('some-resource')
+await patty.register('some-other-resource', handleIt)
+await patty.start()
+*/
