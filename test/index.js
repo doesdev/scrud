02c745153f4f121982db52def9a68c8eca0d9f27
Andrew Carpenter <andrew@audioinhd.com>
2017-02-22 17:46:05 -0500

    Fix helper specs

--- test/index.js
+++ test/index.js
@@ -30,8 +30,8 @@ await patty.start()
 
 /* GLOBAL HELPERS
 const logger = patty.logger // {debug, info, warn, fatal}
-let records = patty._find('some-resource', {id: 1})
-let records = patty._findAll('some-resource', {name: 'jerry'})
+let record = await patty._find('some-resource', {id: 1})
+let records = await patty._findAll('some-resource', {name: 'jerry'})
 let newRecord = await patty._create('some-resource', {name: 'jimmy'})
 let updatedRecord = await patty._save('some-resource', {id: 1, name: 'john'})
 */
