7d9f46b64c4a1c15fabd98807d185aeb25a0657c
Andrew Carpenter <andrew@audioinhd.com>
2017-02-22 17:14:41 -0500

    Add resource object specification

--- test/index.js
+++ test/index.js
@@ -6,13 +6,23 @@ import test from 'ava'
 // test something
 test.todo('testSomething')
 
-/*
+/* API
 const pattyOpts = {port: 8081}
 const patty = require('paternity')
 const handleIt = require('./some-other-resource')
 
 await patty.init(pattyOpts)
-await patty.register('some-resource')
-await patty.register('some-other-resource', handleIt)
+const someResource = await patty.register('some-resource')
+const someOtherResource = await patty.register('some-other-resource', handleIt)
 await patty.start()
+*/
+
+/* RESOURCE OBJECT
+{
+  search: () => {},
+  create: () => {},
+  read: () => {},
+  update: () => {},
+  delete: () => {}
+}
 */
