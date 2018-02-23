4b8ec72696969d6b2a2f79e465f3a9588eff879b
Andrew Carpenter <andrew@audioinhd.com>
2018-02-22 19:32:55 -0500

    Fork all bench servers into child processes

--- bench/index.js
+++ bench/index.js
@@ -1,22 +1,23 @@
 'use strict'
 
-const http = require('http')
+const { fork } = require('child_process')
+const { join } = require('path')
 const getScrud = require('get-scrud')
-const polka = require('polka')
-const fastify = require('fastify')()
-const scrud = require('./../index')
-const express = require('express')
 const autocannon = require('autocannon')
 const table = require('tty-table')
 const ports = {http: 3010, fastify: 3011, polka: 3012, scrud: 3013, express: 3014}
-const ready = {}
-Object.keys(ports).forEach((k) => { ready[k] = false })
-const logStart = (n) => {
-  ready[n] = true
-  if (Object.keys(ready).every((k) => ready[k])) bench()
-}
 const results = []
 const benchId = 301
+Promise.all(Object.keys(ports).map((k) => new Promise((resolve, reject) => {
+  let child = fork(join(__dirname, 'server'), [k])
+  child.once('error', (err) => {
+    console.log(err)
+    process.exit()
+  })
+  child.once('message', (m) => {
+    (m === k) ? resolve() : reject(m)
+  })
+}))).then(() => bench())
 
 const urlTemplate = (port, string) => {
   let url = {host: 'localhost', port, path: `/bench/${benchId}`}
@@ -42,32 +43,6 @@ const formatBytes = (bytes) => {
   return parseFloat((bytes / Math.pow(k, i)).toFixed(dm)) + ' ' + sizes[i]
 }
 
-// HTTP
-http.createServer((req, res) => {
-  res.setHeader('Content-Type', 'application/json')
-  res.end(JSON.stringify({data: `${benchId}`, error: null}))
-}).listen(ports.http, () => logStart('http'))
-
-// FASTIFY
-fastify.get('/bench/:id', function (req, reply) {
-  reply.send({data: `${req.params.id}`, error: null})
-})
-fastify.listen(ports.fastify, () => logStart('fastify'))
-
-// POLKA
-polka().get('/bench/:id', (req, res) => {
-  res.end(JSON.stringify({data: `${req.params.id}`, error: null}))
-}).listen(ports.polka).then(() => logStart('polka'))
-
-// SCRUD
-scrud.register('bench', {read: (req, res) => scrud.sendData(res, `${req.id}`)})
-scrud.start({port: ports.scrud}).then(() => logStart('scrud'))
-
-// EXPRESS
-express().get('/bench/:id', (req, res) => {
-  res.end(JSON.stringify({data: `${req.params.id}`, error: null}))
-}).listen(ports.express, () => logStart('express'))
-
 // benchamrks
 const bencher = (title) => new Promise((resolve, reject) => {
   console.log(`benchmarking ${title}`)
@@ -77,7 +52,12 @@ const bencher = (title) => new Promise((resolve, reject) => {
     results.push(res)
     return resolve(title)
   }
-  let acOpts = {url: urlTemplate(port, true), title, connections: 50}
+  let acOpts = {
+    url: urlTemplate(port, true),
+    title,
+    connections: 50,
+    pipelining: 10
+  }
   autocannon(Object.assign({duration: 3}, acOpts), () => {
     autocannon(Object.assign({duration: 7}, acOpts), done)
   })
---
+++ bench/server.js
@@ -0,0 +1,40 @@
+'use strict'
+
+const ports = {http: 3010, fastify: 3011, polka: 3012, scrud: 3013, express: 3014}
+const logStart = (n) => process.send(n)
+const benchId = 301
+const start = {
+  http: () => {
+    const http = require('http')
+    http.createServer((req, res) => {
+      res.setHeader('Content-Type', 'application/json')
+      res.end(JSON.stringify({data: `${benchId}`, error: null}))
+    }).listen(ports.http, () => logStart('http'))
+  },
+  fastify: () => {
+    const fastify = require('fastify')()
+    fastify.get('/bench/:id', function (req, reply) {
+      reply.send({data: `${req.params.id}`, error: null})
+    })
+    fastify.listen(ports.fastify, () => logStart('fastify'))
+  },
+  polka: () => {
+    const polka = require('polka')
+    polka().get('/bench/:id', (req, res) => {
+      res.end(JSON.stringify({data: `${req.params.id}`, error: null}))
+    }).listen(ports.polka).then(() => logStart('polka'))
+  },
+  scrud: () => {
+    const scrud = require('./../index')
+    scrud.register('bench', {read: (req, res) => scrud.sendData(res, `${req.id}`)})
+    scrud.start({port: ports.scrud}).then(() => logStart('scrud'))
+  },
+  express: () => {
+    const express = require('express')
+    express().get('/bench/:id', (req, res) => {
+      res.end(JSON.stringify({data: `${req.params.id}`, error: null}))
+    }).listen(ports.express, () => logStart('express'))
+  }
+}
+
+start[process.argv[2]]()
