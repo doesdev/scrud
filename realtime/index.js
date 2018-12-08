e0d130cdbca8fd1a570b40d4c98ccecfe86fd553
Andrew Carpenter <andrew@audioinhd.com>
2018-12-08 14:28:15 -0500

    Update deps of submodules

--- realtime/index.js
+++ realtime/index.js
@@ -1,19 +1,19 @@
 'use strict'
 
 const { join } = require('path')
-const ngoOpts = {goPath: join(__dirname, 'gopath')}
+const ngoOpts = { goPath: join(__dirname, 'gopath') }
 const ngo = require('ngo')
 const EMITTER_LISTEN = `:6788`
 let broker
 
-async function startBroker (brokerOpts = {EMITTER_LISTEN}) {
+async function startBroker (brokerOpts = { EMITTER_LISTEN }) {
   await ngo(ngoOpts)(['get', 'github.com/emitter-io/emitter'])
   let emitter = ngo(ngoOpts).bin('emitter')
   let { stderr } = await emitter()
   brokerOpts.EMITTER_LICENSE = stderr.match(/license: (.+)/)[1]
   // let key = stderr.match(/key: (.+)/)[1]
   let getBroker = () => {
-    broker = ngo(Object.assign({env: brokerOpts}, ngoOpts)).bin('emitter')()
+    broker = ngo(Object.assign({ env: brokerOpts }, ngoOpts)).bin('emitter')()
     broker.on('exit', getBroker)
   }
   getBroker()
