f916e6f36663dd1c14fa2c537ed11d4b0567c76e
Andrew Carpenter <andrew@audioinhd.com>
2018-12-18 17:28:29 -0500

    Tests working for non-turbo and turbo

--- test.js
+++ test.js
@@ -3,6 +3,7 @@
 import test from 'ava'
 import requireFresh from 'import-fresh'
 import axios from 'axios'
+import path from 'path'
 import getScrud from 'get-scrud'
 const allowOrigins = ['localhost']
 const postBody = {
@@ -31,21 +32,45 @@ const opts = {
 }
 Object.assign(opts, require('./../_secrets/scrud/config.json'))
 
-for (let turbo of [false]) {
-  const scrud = requireFresh('./index')
+const config = {
+  http: { port: 8092, turbo: false },
+  turbo: { port: 8093, turbo: true }
+}
+const ports = { http: config.http.port, turbo: config.turbo.port }
+
+test.before(async () => {
+  config.http.scrud = requireFresh(path.resolve(__dirname, 'index.js'))
+  config.turbo.scrud = requireFresh(path.resolve(__dirname, 'index.js'))
+
+  await config.http.scrud.register('member')
+  await config.turbo.scrud.register('member')
+
+  const optsHttp = Object.assign({}, opts, config.http)
+  const optsTurbo = Object.assign({}, opts, config.turbo)
+  await config.http.scrud.start(optsHttp)
+  await config.turbo.scrud.start(optsTurbo)
+
+  config.http.jwt = await config.http.scrud.genToken({ some: 'stuffs' })
+  config.turbo.jwt = await config.turbo.scrud.genToken({ some: 'stuffs' })
+
+  config.http.apiCall = getScrud(Object.assign({}, apiOpts, config.http))
+  config.turbo.apiCall = getScrud(Object.assign({}, apiOpts, config.turbo))
+})
+
+const getConfig = (turbo) => {
+  const configKey = turbo ? 'turbo' : 'http'
+  const { scrud, port, apiCall, jwt } = config[configKey]
   const { sendData } = scrud
-  let id, apiCall, jwt
-  const port = opts.port = apiOpts.port = turbo ? 8093 : 8092
+  if (port !== ports[configKey]) throw new Error(`Ports not matching`)
+  return { scrud, port, apiCall, jwt, sendData }
+}
+
+for (let turbo of [false, true]) {
+  let id
   const pre = turbo ? `turbo: ` : ''
 
-  test.before(async () => {
-    await scrud.register('member')
-    await scrud.start(Object.assign({}, opts, { turbo }))
-    jwt = await scrud.genToken({ some: 'stuffs' })
-    apiCall = getScrud(Object.assign({ jwt }, apiOpts))
-  })
-
   test.serial(`${pre}CREATE`, async (assert) => {
+    const { apiCall } = getConfig(turbo)
     // create first so that we can expect data in other actions
     let c = await apiCall('member', 'create', postBody)
     id = c.id
@@ -53,51 +78,59 @@ for (let turbo of [false]) {
   })
 
   test.serial(`${pre}SEARCH`, async (assert) => {
+    const { apiCall } = getConfig(turbo)
     let s = await apiCall('member', 'search', { first: 'andrew' })
     assert.true(Array.isArray(s) && s.length > 0)
   })
 
   test.serial(`${pre}READ`, async (assert) => {
+    const { apiCall } = getConfig(turbo)
     let r = await apiCall('member', 'read', id)
     assert.is(r.zip, '37601')
   })
 
   test.serial(`${pre}UPDATE`, async (assert) => {
+    const { apiCall } = getConfig(turbo)
     let u = await apiCall('member', 'update', id, putBody)
     assert.is(u.zip, '37615')
   })
 
   test.serial(`${pre}DELETE`, async (assert) => {
+    const { apiCall } = getConfig(turbo)
     await assert.notThrowsAsync(apiCall('member', 'delete', id))
   })
 
   test.serial(`${pre}missing resource id returns 404`, async (assert) => {
+    const { port, jwt } = getConfig(turbo)
     let url = `http://localhost:${port}${basePath}/member/`
     let headers = { Authorization: `Bearer ${jwt}` }
     try {
       await axios({ method: 'PUT', url, data: putBody, headers })
     } catch (ex) {
       if (!ex.response) throw ex
-      assert.is(ex.response.status, 404, ex)
+      assert.is(ex.response.status, 404)
     }
   })
 
   test.serial(`${pre}bad JSON body returns error`, async (assert) => {
+    const { port, jwt } = getConfig(turbo)
     let url = `http://localhost:${port}${basePath}/member/${id}`
     let headers = { Authorization: `Bearer ${jwt}` }
     try {
       await axios({ method: 'PUT', url, data: 'u', headers })
     } catch (ex) {
       if (!ex.response) throw ex
-      assert.is(ex.response.status, 500, ex)
+      assert.is(ex.response.data.error, 'Error parsing JSON request body')
     }
   })
 
   test.serial(`${pre}register throws with no name`, async (assert) => {
+    const { scrud } = getConfig(turbo)
     await assert.throwsAsync(scrud.register(), Error, 'register throws with no name')
   })
 
   test.serial(`${pre}register returns resource object`, async (assert) => {
+    const { scrud } = getConfig(turbo)
     let resource = await scrud.register('profile')
     assert.truthy(resource, 'resource is defined')
     assert.truthy(resource.hasOwnProperty('name'), 'resource has name')
@@ -105,6 +138,7 @@ for (let turbo of [false]) {
   })
 
   test.serial(`${pre}exported resource DB helpers work as expected`, async (assert) => {
+    const { scrud } = getConfig(turbo)
     let locId = (await scrud.insert('member', { params: { zip: 37615 } })).id
     assert.is((await scrud.findAll('member', { params: { id: locId } }))[0].zip, '37615')
     await assert.notThrowsAsync(scrud.save('member', { id: locId, params: { zip: '37610' } }))
@@ -113,6 +147,7 @@ for (let turbo of [false]) {
   })
 
   test.serial(`${pre}exported SCRUD helpers work as expected`, async (assert) => {
+    const { scrud } = getConfig(turbo)
     let locId = (await scrud.create('member', { params: { zip: 37615 } })).id
     assert.is((await scrud.search('member', { params: { id: locId } }))[0].zip, '37615')
     await assert.notThrowsAsync(scrud.update('member', { id: locId, params: { zip: 37610 } }))
@@ -121,6 +156,7 @@ for (let turbo of [false]) {
   })
 
   test.serial(`${pre}basePth and path edge cases are handled properly`, async (assert) => {
+    const { scrud, port, jwt, sendData } = getConfig(turbo)
     let hdl = (req, res) => Promise.resolve(sendData(res, 'test'))
     let handlers = { search: hdl, create: hdl, read: hdl, update: hdl, delete: hdl }
     await scrud.register('api', handlers)
